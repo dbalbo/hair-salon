ea3461e5885720c6270410482408cc76b84675cb
Debbie <debbalbo@hotmail.com>
2015-05-08 09:31:01 -0700

    created name and id specs and code to be able to name and set id number to a stylist

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -6,5 +6,58 @@ require('rubygems')
 
 
 describe(Stylist)  do
+
+
+	describe('#name') do
+		it('returns the stylist name') do
+			stylist = Stylist.new ({:name => 'Brenda', :id => nil})
+			expect(stylist.name()).to(eq('Brenda'))
+		end
+	end
+
+	describe('#id') do
+		it('returns and sets an id number for each stylist') do
+			stylist = Stylist.new({:name => 'Brenda', :id => 1})
+			expect(stylist.id()).to(eq(1))
+		end
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 end
